0e1f322866e5c7dc51dfcb3c6018c112409768ff
Michael El Omar <michaelelomar@outlook.com>
2026-04-05 22:59:26 +0200

    add atom.mk file

---
+++ atom.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+################################################################################
+# sdformat
+################################################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := sdformat
+LOCAL_DESCRIPTION := Robot and simulation model format.
+LOCAL_CATEGORY_PATH := lib
+
+LOCAL_LIBRARIES := \
+	gz-math \
+	gz-utils
+
+LOCAL_EXPORT_LDLIBS := -lsdformat16
+
+LOCAL_CXXFLAGS := -std=c++17
+# LOCAL_EXPORT_C_INCLUDES := $(TARGET_OUT_STAGING)/usr/include/sdformat-9.10
+
+LOCAL_CMAKE_CONFIGURE_ARGS := \
+	-DBUILD_TESTING:BOOL=False \
+	-DUSE_UPSTREAM_CFLAGS:BOOL=False \
+	-DIGNITION-MATH_INCLUDE_DIRS=$(TARGET_OUT_STAGING)/usr/gz/math2 \
+	-DIGNITION-MATH_LIBRARIES=$(TARGET_OUT_STAGING)/usr/lib/libgz-math2.so \
+	-DIGNITION-MATH_LIBRARY_DIRS=$(TARGET_OUT_STAGING)/usr/lib
+
+include $(BUILD_CMAKE)
+
+
